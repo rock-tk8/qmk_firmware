acdbcb53f54019be825187263e83b735681ae619
Iwahira Takuya <takuya@Minerva.local>
2020-03-12 01:16:55 +0900

    [wip] new layout

--- keyboards/ergodash/mini/keymaps/tk8/keymap.c
+++ keyboards/ergodash/mini/keymaps/tk8/keymap.c
@@ -29,16 +29,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |   [  |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |NUMPAD|                    | Bksp |   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | Del  |                    | Bksp |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * |  Esc |  Alt |  GUI | EISU |||||||| Lower|  F10 | Space|||||||| Space| Enter| Raise|||||||| Kana | GUI  | Home |  End |
+   * |  Esc |  Alt |  Alt |  GUI |||||||| Lower| EISU | Space|||||||| Enter| Kana | Raise||||||||  GUI |  Alt | Home |  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    NUMPAD,                         KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LALT, KC_LGUI, EISU,            LOWER,    KC_F10,  KC_SPC,        KC_SPC, KC_ENT , RAISE,            KANA,    KC_RGUI, KC_HOME, KC_END   \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                         KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_ESC,  NUMPAD,  KC_LALT, KC_LGUI,          LOWER,   EISU,    KC_SPC,        KC_ENT, KANA,    RAISE,            KC_RGUI, KC_RALT, KC_HOME, KC_END   \
   ),
 
   /* Lower
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,                        KC_RCBR, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, KC_PGUP, XXXXXXX, \
-    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,                        _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, \
+    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,                        _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, \
     _______, _______, _______, _______,          LOWER,   _______,_______,        _______,_______, RAISE,            _______, _______, _______, _______  \
   ),
 
@@ -67,14 +67,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
   * |      |   F6 |   F7 |   F8 |   F9 |  F10 ||||||||                    |||||||| BRIUP| BRIDN| MUTE |||||||||||||||SCRST1|
   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-  * |      |      |      | EISU |||||||| Lower||||||||      ||||||||      |      | Raise||||||||      |      |      |SCRST2|
+  * |      |      |      | 　　　|||||||| Lower|      |      ||||||||      |      | Raise||||||||      |      |      |SCRST2|
   * ,----------------------------------------------------------------------------------------------------------------------.
   */
   [_RAISE] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        XXXXXXX, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, \
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,                        XXXXXXX, KC_BRIU, KC_BRID, KC_MUTE, XXXXXXX, XXXXXXX, KC_SS1,  \
-    _______, _______, _______, _______,          LOWER,   XXXXXXX,_______,        _______,_______, RAISE,            _______, _______, _______, KC_SS2   \
+    _______, _______, _______, _______,          LOWER,   _______,_______,        _______,_______, RAISE,            _______, _______, _______, KC_SS2   \
     ),
 
   /* NUMPAD
